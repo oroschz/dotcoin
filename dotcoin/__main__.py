59d32fd85bc56ead4acc2b5675d3a8d312c295dd
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-05-13 20:32:16 -0500

    refactor: clean restart for next iteration.

--- dotcoin/__main__.py
+++
@@ -1,4 +0,0 @@
-from dotcoin.console import main
-
-if __name__ == '__main__':
-    main()
--- dotcoin/core/error.py
+++
@@ -1,6 +0,0 @@
-class InvalidSourceError(Exception):
-    """Requested source does not exist"""
-
-
-class InvalidContextError(Exception):
-    """Requested context does not exist"""
